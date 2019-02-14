6cd8cdec814a0e857b69baeabec1b9563feeeb01
Mr. Bartucz <jobartucz@isd535.org>
2019-02-13 21:03:07 -0600

    needed to be arr.clone() on line 66

--- Sort.java
+++ Sort.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Main 
 { 
@@ -61,7 +61,7 @@ class Main
         System.out.println(Arrays.toString(arr));
         
         // create a sorted version to test.
-        double sortedArr[] = arr;
+        double sortedArr[] = arr.clone();
         Arrays.sort(sortedArr);
         System.out.println("sorted array: "); 
         System.out.println(Arrays.toString(sortedArr));
